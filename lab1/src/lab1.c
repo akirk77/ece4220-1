ccf8b4dc9f2f8460c905afb9523b2c6773ca6e54
Joel Abshier <njamb7@mail.missouri.edu>
2018-01-25 14:37:52 -0600

    Test Push Lab1

---
+++ lab1/src/lab1.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : ECE 4220: Lab 1
+ Author      : Joel Abshier
+ Version     : 1.0.0
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+
+int main(void)
+{
+
+	puts("Begin Lab1");
+
+	return 0;
+
+}
