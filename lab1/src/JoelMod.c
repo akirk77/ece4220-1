c08b123f3795e02e5def4fa8ef4a6ae567d37fb6
Joel Abshier <31899323+abshierjoel@users.noreply.github.com>
2018-02-06 16:09:36 -0600

    Added Comments to the code

--- lab1/src/JoelMod.c
+++ lab1/src/JoelMod.c
@@ -17,27 +17,42 @@ MODULE_AUTHOR( "Joel Abshier" );
 
 int joelmod_init( void )
 {
+	//Map the location of the GPIO pins in memory
 	unsigned long * selpin = (unsigned long*) ioremap( (0x3F200000), 4096 );
+	
+	//Set the pin modes to output for each LED
 	iowrite32( (*selpin | 0x00049240) , selpin );
 
+	//Move the Pin pointer forward 7 * (4 bits) to the pins for setting 1 
 	selpin = selpin + 7;
-    iowrite32( (0x0000003C), selpin );
+	
+	//Set the write pin to 1
+   	iowrite32( (0x0000003C), selpin );
 
-    printk( "JoelMod Installed\n\n" );
+	//Output niceness
+    	printk( "JoelMod Installed\n\n" );
 
-    return 0;
+    	return 0;
 }
 
 void joelmod_cleanup( void )
 {
+	//Map the location of the GPIO pins in memory
 	unsigned long * selpin = (unsigned long*) ioremap( (0x3F200000), 4096 );
-    iowrite32( (*selpin | 0x00049240) , selpin );
+    	
+	//Set the pin modes to output for each LED
+	iowrite32( (*selpin | 0x00049240) , selpin );
 
-    selpin = (selpin + 10);
-    iowrite32( 0x0000003C, selpin );
+	//Move the pin pointer forware 10 * (4 bits) to the pins for clearing 1
+    	selpin = (selpin + 10);
+	
+	//Set the clear pin to 0
+    	iowrite32( 0x0000003C, selpin );
 
-    printk( "JoelMod Removed\n\n" );
+	//Output niceness
+    	printk( "JoelMod Removed\n\n" );
 }
 
+//Make the mode init and exit functions link to mine.
 module_init( joelmod_init );
 module_exit( joelmod_cleanup );
