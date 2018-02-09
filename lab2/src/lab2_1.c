1e437b27912dbb21b79d1bff5300285e97785ee2
Joel Abshier <njamb7@mail.missouri.edu>
2018-02-09 17:03:26 -0600

    Finished Lab2 Code

--- lab2/src/lab2_1.c
+++ lab2/src/lab2_1.c
@@ -18,6 +18,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <time.h>
 
 typedef struct
 {
@@ -69,25 +70,20 @@ void search_rows( void * ptr )
 
 	thread * thread_info = (thread * ) ptr;
 	datas * dat = thread_info->dat;
+	fflush( stdout );
 	int id = thread_info->thread_id;
 
 	int j;
 	int search = (*dat).search;
 	int cols = (*dat).cols;
 
-	//printf( "\n%3d: ", id );
-	fflush( stdout );
-
 	for( j = 0; j < cols; j++ )
 	{
-		//printf( "%3d ", (*dat).data_matrix[id][j] );
 		if( (*dat).data_matrix[id][j] == search )
 		{
 			++(*dat).count[id];
 		}
 	}
-
-	//printf( "\nFound: %d\n\n\n", (*dat).count[id] );
 
 	//Kill thread!
 	pthread_exit( 0 );
@@ -98,14 +94,47 @@ void search_rows( void * ptr )
 void search_cols( void * ptr )
 {
 
-	//datas * dat = (datas*) ptr;
+	thread * thread_info = (thread * ) ptr;
+	datas * dat = thread_info->dat;
+	int id = thread_info->thread_id;
+
+	int j;
+	int search = (*dat).search;
+	int rows = (*dat).rows;
+
+	for( j = 0; j < rows; j++ )
+	{
+		if( (*dat).data_matrix[j][id] == search )
+		{
+			++(*dat).count[id];
+		}
+	}
+
+	//Kill thread!
+	pthread_exit( 0 );
 
 }
 
 void search_elems( void * ptr )
 {
 
-	//datas * dat = (datas*) ptr;
+	thread * thread_info = (thread * ) ptr;
+	datas * dat = thread_info->dat;
+	int id = thread_info->thread_id;
+
+	int search = (*dat).search;
+	int col = id / (*dat).rows;
+	int row = id % (*dat).rows;
+
+	//printf( "\nR: %d; C: %d; V: %d\n", row, col, (*dat).data_matrix[row][col] );
+
+	if( (*dat).data_matrix[row][col] == search )
+	{
+		++(*dat).count[id];
+	}
+
+	//Kill thread!
+	pthread_exit( 0 );
 
 }
 
@@ -182,10 +211,20 @@ int main(int argc, char ** argv )
 		printf( "\n" );
 	}*/
 
+	//Timer structs
+	struct timespec startTime, endTime;
+	unsigned int diff;
+
 	//START SEARCH WITH ONE THREAD
+	clock_gettime( CLOCK_MONOTONIC, &startTime );
+
 	pthread_t threadOne;
 	pthread_create( &threadOne, NULL, (void *) &search_one, (void *) &theData);
 	pthread_join( threadOne, NULL );
+
+	clock_gettime( CLOCK_REALTIME, &endTime );
+	diff = 1000000000 * (endTime.tv_sec - startTime.tv_sec) + endTime.tv_nsec - startTime.tv_nsec;
+	printf( "\nOne Thread search finished in %u ns", diff );
 
 	//END SEARCH WITH ONE THREAD
 
@@ -196,6 +235,8 @@ int main(int argc, char ** argv )
 	//START SEARCH WITH THREADS PER ROWS
 	pthread_t threadRows[rows];
 	thread thread_data[rows];
+
+	clock_gettime( CLOCK_MONOTONIC, &startTime );
 
 	int k;
 	for( k = 0; k < rows; k++ )
@@ -209,6 +250,11 @@ int main(int argc, char ** argv )
 	{
 		pthread_join( threadRows[k], NULL );
 	}
+
+	clock_gettime( CLOCK_REALTIME, &endTime );
+	diff = 1000000000 * (endTime.tv_sec - startTime.tv_sec) + endTime.tv_nsec - startTime.tv_nsec;
+	printf( "\nThreads per rows search finished in %u ns", diff );
+
 
 	int rowCount = 0;
 	for( w = 0; w < 225; w++ ) { rowCount += theData.count[w]; }
@@ -224,29 +270,60 @@ int main(int argc, char ** argv )
 	pthread_t threadCols[cols];
 	thread thread_datac[cols];
 
-	for( k = 0; k < rows; k++ )
+	clock_gettime( CLOCK_MONOTONIC, &startTime );
+	for( k = 0; k < cols; k++ )
 	{
 		//printf( "\n\nCreating Thread: %d", k );
 		thread_datac[k].thread_id = k;
 		thread_datac[k].dat = &theData;
-		pthread_create( &threadCols[k], NULL, (void *) &search_rows, (void *) &thread_datac[k] );
+		pthread_create( &threadCols[k], NULL, (void *) &search_cols, (void *) &thread_datac[k] );
 	}
-	for( k = 0; k < rows; k++ )
+	for( k = 0; k < cols; k++ )
 	{
 		pthread_join( threadCols[k], NULL );
 	}
 
+	clock_gettime( CLOCK_REALTIME, &endTime );
+	diff = 1000000000 * (endTime.tv_sec - startTime.tv_sec) + endTime.tv_nsec - startTime.tv_nsec;
+	printf( "\nThreads per column search finished in %u ns", diff );
+
+
 	int colCount = 0;
-	for( w = 0; w < 225; w++ ) { rowCount += theData.count[w]; }
-	printf( "\n\n>> Found %d instances of %d in row search.\n", colCount, theData.search );
+	for( w = 0; w < 225; w++ ) { colCount += theData.count[w]; }
+	printf( "\n\n>> Found %d instances of %d in column search.\n", colCount, theData.search );
 
 	//END SEARCH WITH THREADS PER COLUMNS
 
 
+	//Reset count
+	for( w = 0; w < 225; w++ ) { theData.count[w] = 0; }
+
+
 	//START SEARCH WITH THREADS PER ELEMENT
-	pthread_t threadElems;
-	pthread_create( &threadElems, NULL, (void *) &search_elems, (void *) &theData);
-	pthread_join( threadElems, NULL );
+	int elems = cols * rows;
+	pthread_t threadElems[elems];
+	thread thread_datae[elems];
+
+	clock_gettime( CLOCK_MONOTONIC, &startTime );
+	for( k = 0; k < elems; k++ )
+	{
+		//printf( "\nCreating Thread: %d", k );
+		thread_datae[k].thread_id = k;
+		thread_datae[k].dat = &theData;
+		pthread_create( &threadElems[k], NULL, (void *) &search_elems, (void *) &thread_datae[k] );
+	}
+	for( k = 0; k < elems; k++ )
+	{
+		pthread_join( threadElems[k], NULL );
+	}
+
+	clock_gettime( CLOCK_REALTIME, &endTime );
+	diff = 1000000000 * (endTime.tv_sec - startTime.tv_sec) + endTime.tv_nsec - startTime.tv_nsec;
+	printf( "\nThreads per element search finished in %u ns", diff );
+
+	int elemCount = 0;
+	for( w = 0; w < 225; w++ ) { elemCount += theData.count[w]; }
+	printf( "\n\n>> Found %d instances of %d in element search.\n", elemCount, theData.search );
 
 	//END SEARCH WITH THREADS PER ELEMENT
 
