95ff50f36d0925186e2e4aa5d84f18c71b34e6d1
Joel Abshier <njamb7@mail.missouri.edu>
2018-02-09 12:51:33 -0600

    Added Row Search

--- lab2/src/lab2_1.c
+++ lab2/src/lab2_1.c
@@ -24,7 +24,15 @@ typedef struct
 	int search;
 	int data_matrix[20][100];
 	int count[255];
+	int rows;
+	int cols;
 } datas;
+
+typedef struct
+{
+	int thread_id;
+	datas * dat;
+} thread;
 
 void search_one( void * ptr )
 {
@@ -32,6 +40,23 @@ void search_one( void * ptr )
 	datas * dat = (datas*) ptr;
 
 	printf( "\n\n>> Searching for %d with one thread.\n", (*dat).search );
+
+	int i, j;
+	int search = (*dat).search;
+	int rows = (*dat).rows;
+	int cols = (*dat).cols;
+	for( i = 0; i < rows; i++ )
+	{
+		for( j = 0; j < cols; j++ )
+		{
+			if( (*dat).data_matrix[i][j] == search )
+			{
+				++(*dat).count[0];
+			}
+		}
+	}
+
+	printf( "\n\n>> Found %d instances of %d.\n", (*dat).count[0], search );
 
 	//Kill thread!
 	pthread_exit( 0 );
@@ -42,7 +67,30 @@ void search_one( void * ptr )
 void search_rows( void * ptr )
 {
 
-	//datas * dat = (datas*) ptr;
+	thread * thread_info = (thread * ) ptr;
+	datas * dat = thread_info->dat;
+	int id = thread_info->thread_id;
+
+	int j;
+	int search = (*dat).search;
+	int cols = (*dat).cols;
+
+	//printf( "\n%3d: ", id );
+	fflush( stdout );
+
+	for( j = 0; j < cols; j++ )
+	{
+		//printf( "%3d ", (*dat).data_matrix[id][j] );
+		if( (*dat).data_matrix[id][j] == search )
+		{
+			++(*dat).count[id];
+		}
+	}
+
+	//printf( "\nFound: %d\n\n\n", (*dat).count[id] );
+
+	//Kill thread!
+	pthread_exit( 0 );
 
 
 }
@@ -98,28 +146,27 @@ int main(int argc, char ** argv )
 	puts( "\nFile opened" );
 
 	puts( "Reading rows and columns ");
-	int rows = 20;
-	int cols = 100;
+	int rows, cols;
 
-	puts( "Read rows");
 	fflush( stdout );
 	fscanf( file, " %d", &rows );
-	puts( "Read cols");
 	fscanf( file, " %d", &cols );
 
+	theData.rows = rows;
+	theData.cols = cols;
 
-	/*puts( "Loop and load data!");
-	fflush( stdout );
+
+	puts( "Loop and load data!");
 	int i,j;
 	for( i = 0; i < rows; i++ )
 	{
-		printf( "\n%d: ", i );
+		//printf( "\n%2d: ", i );
 		for( j = 0; j < cols; j++ )
 		{
-			printf( "%d ", j );
 			fscanf( file, " %d", &theData.data_matrix[i][j] );
+			//printf( "%5d", theData.data_matrix[i][j] );
 		}
-	}*/
+	}
 
 	//Close the file
 	fclose( file );
@@ -130,7 +177,7 @@ int main(int argc, char ** argv )
 	{
 		for( j = 0; j < cols; j++ )
 		{
-			printf( "%d ", theData.data_matrix[i][j] );
+			printf( "%5d", theData.data_matrix[i][j] );
 		}
 		printf( "\n" );
 	}*/
@@ -142,19 +189,56 @@ int main(int argc, char ** argv )
 
 	//END SEARCH WITH ONE THREAD
 
+	//Reset count
+	int w = 0;
+	for( w = 0; w < 225; w++ ) { theData.count[w] = 0; }
 
 	//START SEARCH WITH THREADS PER ROWS
-	pthread_t threadRows;
-	pthread_create( &threadRows, NULL, (void *) &search_rows, (void *) &theData);
-	pthread_join( threadRows, NULL );
+	pthread_t threadRows[rows];
+	thread thread_data[rows];
+
+	int k;
+	for( k = 0; k < rows; k++ )
+	{
+		//printf( "\n\nCreating Thread: %d", k );
+		thread_data[k].thread_id = k;
+		thread_data[k].dat = &theData;
+		pthread_create( &threadRows[k], NULL, (void *) &search_rows, (void *) &thread_data[k] );
+	}
+	for( k = 0; k < rows; k++ )
+	{
+		pthread_join( threadRows[k], NULL );
+	}
+
+	int rowCount = 0;
+	for( w = 0; w < 225; w++ ) { rowCount += theData.count[w]; }
+	printf( "\n\n>> Found %d instances of %d in row search.\n", rowCount, theData.search );
 
 	//END SEARCH WITH THREADS PER ROWS
 
+	//Reset count
+	for( w = 0; w < 225; w++ ) { theData.count[w] = 0; }
+
 
 	//START SEARCH WITH THREADS PER COLUMNS
-	pthread_t threadCols;
-	pthread_create( &threadCols, NULL, (void *) &search_cols, (void *) &theData);
-	pthread_join( threadCols, NULL );
+	pthread_t threadCols[cols];
+	thread thread_datac[cols];
+
+	for( k = 0; k < rows; k++ )
+	{
+		//printf( "\n\nCreating Thread: %d", k );
+		thread_datac[k].thread_id = k;
+		thread_datac[k].dat = &theData;
+		pthread_create( &threadCols[k], NULL, (void *) &search_rows, (void *) &thread_datac[k] );
+	}
+	for( k = 0; k < rows; k++ )
+	{
+		pthread_join( threadCols[k], NULL );
+	}
+
+	int colCount = 0;
+	for( w = 0; w < 225; w++ ) { rowCount += theData.count[w]; }
+	printf( "\n\n>> Found %d instances of %d in row search.\n", colCount, theData.search );
 
 	//END SEARCH WITH THREADS PER COLUMNS
 
